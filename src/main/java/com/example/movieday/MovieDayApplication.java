1960186e06586a3a94490aff7f8a8ee6509679c8
Husamettin Arslan <husamettinarslan@hotmail.com>
2020-03-03 23:08:49 +0100

    initial commit ongoing

---
+++ src/main/java/com/example/movieday/MovieDayApplication.java
@@ -0,0 +1,13 @@
+package com.example.movieday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieDayApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MovieDayApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/movieday/MovieDayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.movieday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieDayApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
